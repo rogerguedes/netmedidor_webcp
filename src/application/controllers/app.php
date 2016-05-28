7bbd0047ffb8c711624bbdaae5ca2c949836ec8f
Rogers <rogerguedes.ft@gmail.com>
2016-05-28 15:23:44 -0300

    Update app.php
    Adding new methods.

--- src/application/controllers/app.php
+++ src/application/controllers/app.php
@@ -4,31 +4,27 @@ class App extends CI_Controller {
 
     public function __construct(){
         parent::__construct();
+        $this->load->helper('form');
         $this->load->library('protection');
         $this->load->library('apphelper');
-        $this->load->library('c3po');
         $this->load->library('form_validation');
-        $this->form_validation->set_error_delimiters('', '');
-        //$this->load->library('debugger'); //$this->debugger->dump($data, true);
     }
-
-    public function index($lang='en-GB'){
+    
+    public function index(){
         $this->apphelper->loadDefaultViewData($data);
-        $data['glossary'] = &$this->c3po->getGlossary($lang);
         if($this->protection->isUserLogged()){
             $this->load->model('model_user');
-            $user = $this->model_user->getUserByEmail( $this->session->userdata('email') );
-            $data['glossary'] = &$this->c3po->getGlossary( $user->getLanguage() );
-            $data['sessionUser'] = $user;
-            $this->load->view('html/view_syncnodes.php',$data);// implementar ADM dashboard
+            $email = $this->session->userdata('email');
+            $data['sessionUser'] = $this->model_user->getUserByEmail($email);
+            $this->load->view('html/dash_board.php',$data);// implementar ADM dashboard
         }
         else{
-            $data['glossaryList'] = $this->c3po->getGlossaryList();
+            $this->apphelper->loadLoginInputs($data);
             $this->load->view('html/view_login',$data);
         }
     }
     
-    public function login($lang='en-GB'){
+    public function login(){
         $data['status'] = null;
         $data['object'] = array();
         $data['errors'] = array();
@@ -40,15 +36,9 @@ class App extends CI_Controller {
         $loginSubmitOk = false;
         $loginCheck = false;
 
-        $data['glossary'] = &$this->c3po->getGlossary($lang);
-
         if( $mayClientTryLogin ){
-            $validationRules = array(
-                array('field' => 'email', 'label' => $data['glossary']['GENERAL']['EMAIL'], 'rules' => 'required'),
-                array('field' => 'password', 'label' => $data['glossary']['GENERAL']['PASSWORD'], 'rules' => 'required')
-            );
-            $this->form_validation->set_rules( $validationRules );
-            $this->form_validation->set_message('required', $data['glossary']['VALIDATION']['REQUIRED']);
+            $this->form_validation->set_rules('email','Email','required');//setting a form_validation rule to the field 'name'
+            $this->form_validation->set_rules('password','Password','required');//setting a form_validation rule to the field 'name'
             $loginSubmitOk = $this->form_validation->run();
             if( $loginSubmitOk ){
                 $email = $this->input->post('email');
@@ -64,17 +54,12 @@ class App extends CI_Controller {
                     $data['status'] = true;
                 }else{
                     $data['status'] = false;
-                    $data['errors'][] = $data['glossary']['MSGS']['BAD_LOGIN'];
+                    $data['errors'][] = "Wrong login or password.";
                     $this->model_security->lookAtThisGuy( $clientAttempts );
                 }
             }else{
                 $data['status'] = false;
-                foreach($validationRules as $field){
-                    $error = form_error($field['field']);
-                    if($error != ""){
-                        $data["errors"][] = $error;
-                    }
-                }
+                $data['errors'][] = "Blank username or login.";
             }
         }
         else{
@@ -88,12 +73,14 @@ class App extends CI_Controller {
             $this->apphelper->loadDefaultViewData($data);
             if( $mayClientTryLogin ){
                 if( $loginSubmitOk && $loginCheck ){
+                        $this->load->library('session');
+                        $this->session->set_userdata('email',$email);
                         $data['sessionUser'] = $this->model_user->getUserByEmail($email);
                         header("Location: ".base_url());
                         exit();
                 }else{
-                    $data['glossaryList'] = $this->c3po->getGlossaryList();
                     $viewName = 'html/view_login';
+                    $this->apphelper->loadLoginInputs($data);
                 }
             }else{
                 $data["blockedTime"] = $this->model_security->getBlockedTime();
@@ -108,20 +95,81 @@ class App extends CI_Controller {
             break;
         default:
             $viewName = 'text_render.php';
-            $data['text'] = $data['glossary']['MSGS']['UNKNOW_MIME'];
+            $data['text'] = $this->apphelper->getErrMsgs()['unknowMIME'];
             break;
         }
         //sends the answer
         $this->load->view($viewName, $data);
     }
     
-    public function logout($lang='en-GB'){
+    public function logout(){
+        $data['status'] = null;
+        $data['object'] = array();
+        $data['errors'] = array();
+        
         $this->load->library('session');
         $this->session->sess_destroy();
-        $this->apphelper->loadDefaultViewData($data);
-        $data['glossary'] = &$this->c3po->getGlossary($lang);
-        $data['glossaryList'] = $this->c3po->getGlossaryList();
-        $this->load->view('html/view_login',$data);
+        
+        $data['status'] = true;
+        
+        $clientAccepts = $this->apphelper->getAcceptHeader();
+        switch($clientAccepts[0]){
+        case "text/html":
+            $this->apphelper->loadDefaultViewData($data);
+            $this->apphelper->loadLoginInputs($data);
+            $viewName = 'html/view_login';
+            break;
+        case "application/json":
+            $viewName = 'json_render.php';
+            $data['jsonData']['status'] = &$data['status'];
+            $data['jsonData']['object'] = &$data['object'];
+            $data['jsonData']['errors'] = &$data['errors'];
+            break;
+        default:
+            $viewName = 'text_render.php';
+            $data['text'] = $this->apphelper->getErrMsgs()['unknowMIME'];
+            break;
+        }
+        //sends the answer
+        $this->load->view($viewName, $data);
+    }
+    
+    public function checkLogin(){
+        $data['status'] = null;
+        $data['object'] = null;
+        $data['errors'] = array();
+        
+        if($this->protection->isUserLogged()){
+            $this->load->model('model_user');
+            $email = $this->session->userdata('email');
+            $data['status'] = true;
+            $data['object'] = $this->model_user->getUserByEmail($email);
+        }
+        else{
+            $data['status'] = false;
+        }
+        
+        //prepare the answer
+        $clientAccepts = $this->apphelper->getAcceptHeader();
+        switch($clientAccepts[0]){
+        case "text/html":
+            $this->apphelper->loadDefaultViewData($data);
+            var_dump($data);
+            exit();
+            break;
+        case "application/json":
+            $viewName = 'json_render.php';
+            $data['jsonData']['status'] = &$data['status'];
+            $data['jsonData']['object'] = &$data['object'];
+            $data['jsonData']['errors'] = &$data['errors'];
+            break;
+        default:
+            $viewName = 'text_render.php';
+            $data['text'] = $this->apphelper->getErrMsgs()['unknowMIME'];
+            break;
+        }
+        //sends the answer
+        $this->load->view($viewName, $data);
     }
 }
 
